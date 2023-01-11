0ea9b7cff7bba3b6882862516005298170f172d6
Jindou Jiang <jindoujiang@outlook.com>
2023-01-11 03:49:49 +0000

    this is a quick change

--- askit/app/urls.py
+++ askit/app/urls.py
@@ -4,5 +4,10 @@ from django.urls import path
 app_name = 'askit'
 
 urlpatterns = [
-    path('', views.main_page, name='main_page'),
+    path('main_page', views.main_page, name='main_page'),
+    path('test', views.test, name='test'),
+    path('new_question', views.new_question, name='new_question'),
+    path('base_page', views.base_page, name='base_page'),
+    # path('search', views.search, name='search'),
+
 ]
--- askit/app/views.py
+++ askit/app/views.py
@@ -1,6 +1,24 @@
 # Create your views here.
 from django.shortcuts import render
 from django.http import HttpResponse
+from app.forms import QuestionForm
+
+def base_page(request):
+  return render(request, "base_page.html")
 
 def main_page(request):
-  return HttpResponse("Bingo!")
+
+  return render(request, "main_page.html")
+
+def test(request):
+  return render(request, "test.html")
+
+def new_question(request):
+  new_question_form = QuestionForm()
+
+  print('The request method is: ', request.method)
+  print('The POST data is:', request.POST)
+  return render(request, "app/templates/new_question.html", {'new_question_form': new_question_form})
+
+# def search(request):
+#   latest_search = 
